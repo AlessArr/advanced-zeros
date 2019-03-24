3ff3b2d88d85b9359c5c6ae532629e45ea6e84eb
AlessArr <lesshere@mail.ru>
2019-03-24 22:54:58 +0300

    feat: edit solution 98/100

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-/*module.exports = function getZerosCount(number, base) {
-
-let count_iter=0, bool=false, div=2, temp=number, sum=0;
-	while(base%div===0)
-	{
-		base/=div;  //разложение на простые множители
-		count_iter++; //количество вхождений простых множителей
-		bool=true;  //новый простой мнодитель учтен
-		console.log(div+'^'+count_iter);
-	}
-	if(bool)  //есть новый простой множитель
-	{
-		console.log('div='+div);
-		while((temp/=div)>=1)    //temp=number, сумма пока рекурентная формула >=1
-		{
-			sum+=temp;
-			console.log('temp='+temp+'  sum='+sum);
-		}
-		sum=Math.floor(sum/count_iter);
-		console.log('end_sum='+sum);
-	}
-	div++; //проверка на делимость на следующий множитель
-	bool=false;
-	sum=0;
-}*/
 module.exports = function getZerosCount(number, base) {
-  // your implementation
   let i=0, j=0;
   let div=2, count_iter=0, sum=0, sum_min=0;
   let bool=false;
@@ -61,13 +35,3 @@ module.exports = function getZerosCount(number, base) {
   }
   return sum_min;
 }
-
-
-/*let zero_count=0;
-  while(number!=0)
-  {
-  	number/=5;
-  	zero_count+=number;
-  }
-  zero_count=Math.floor(zero_count);
-  return zero_count;*/
